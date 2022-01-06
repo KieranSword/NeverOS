a5026c160acd75e5e60e0e74a692d482a2664289
Kieran <kieransword01@hotmal.com>
2022-01-06 18:27:20 +0000

    Made changes

--- src/kernel.h
+++ src/kernel.h
@@ -1,8 +1,17 @@
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #define bootmsg "Booting...\n"
-void boot()
-{
+void boot(){
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(".");
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            printf("%s\n", dir->d_name);
+        }
+        closedir(d);
+    }
     printf("%s",bootmsg);
     system("clear");
 }
