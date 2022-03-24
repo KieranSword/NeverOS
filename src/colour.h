06d45b6a1284993f68eed57510af6f8f3b63f926
Kieran <57414269+KieranSword@users.noreply.github.com>
2022-03-24 22:59:12 +0000

    Delete colour.h
    Not using this anymore

--- src/colour.h
+++
@@ -1,8 +0,0 @@
-#define normal  "\x1B[0m"
-#define red  "\x1B[31m"
-#define green  "\x1B[32m"
-#define yellow "\x1B[33m"
-#define blue "\x1B[34m"
-#define magenta  "\x1B[35m"
-#define cyan  "\x1B[36m"
-#define white "\x1B[37m"
